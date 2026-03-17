083a316ac1be7a187e076ebed1606a5e2bece24f
SantiagoValdezSSV <valdez.sas@obmontessori.edu.ph>
2026-03-17 12:29:23 +0800

    Add files via upload

---
+++ main33.js
@@ -0,0 +1,8 @@
+// Participation activity 2
+function compute_rank() {
+    let math_score = Number(document.getElementById('math_score').value);
+   
+    if (math_score > 94 ) {
+        window.alert('Gold')
+    }
+}
