a27dc37571c632a1b26449004211ab71786ba8c1
SantiagoValdezSSV <valdez.sas@obmontessori.edu.ph>
2026-03-17 12:33:09 +0800

    Update main33.js

--- main33.js
+++ main33.js
@@ -1,8 +1,17 @@
 // Participation activity 2
-function compute_rank() {
-    let math_score = Number(document.getElementById('math_score').value);
-   
-    if (math_score > 94 ) {
+if (math_score >= 95 ) {
         window.alert('Gold')
+    }
+    if (math_score >= 90 ) {
+        window.alert('Silver')
+    }
+    if (math_score >= 85 ) {
+        window.alert('Bronze')
+    }
+    if (math_score >= 80 ) {
+        window.alert('Merit')
+    }
+    if (math_score <= 79 ) {
+        window.alert('Invalid')
     }
 }
